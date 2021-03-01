6814514ebe7b724c9e872fc6d2fcb07af3e3c9b6
Neslihan Kuş Göymener <neslihankus@gmail.com>
2021-03-01 11:11:02 +0300

    14.gün ders sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,11 +11,21 @@ namespace Business.Constants
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string ProductsListed="Ürünler listelendi";
+
         public static string MaintenanceTime="Sistem Bakımda";
         public static string CategoriesListed = "Kategoriler listelendi";
         public static string UnitPriceInvalid = "Fiyat geçersiz";
+
         public static string ProductCountOfCategoryError="Bir kategoride en fazla 10 ürün olabilir";
         public static string ProductNameAlreadyExist = "Aynı isimde ürün eklenemez";
         public static string CategoryLimitExceded = "Kategori sayısı 15 i geçemez, yeni ürün eklenemez";
+
+        public static string AuthorizationDenied="Yetkiniz Yok";
+        public static string UserRegistered="Kayıt Oldu";
+        public static string UserNotFound ="Kullanıcı Bulunamadı";
+        public static string PasswordError="Parola Hatası";
+        public static string SuccessfulLogin="Başarılı Giriş";
+        public static string UserAlreadyExists="Kullanıcı Mevcut";
+        public static string AccessTokenCreated="Token oluşturuldu";
     }
 }
